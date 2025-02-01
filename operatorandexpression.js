d5b290eb5ae14c0f7d6ff97e205dfe887b34873c
adarshkumar8969 <111650723+adarshkumar8969@users.noreply.github.com>
2025-02-01 19:05:01 +0530

    change

---
+++ operatorandexpression.js
@@ -0,0 +1,26 @@
+// basic question
+// 1.What is an expression in JavaScript?
+
+//Answer: An expression is a piece of code that evaluates to a value. It can consist of variables, operators, and literals (e.g., 5 + 3, x > 10).
+/* 2.What are the different types of operators in JavaScript?
+
+Answer: JavaScript has several types of operators, including:
+
+Arithmetic (+, -, *, /, %, **)
+
+Assignment (=, +=, -=, *=, /=)
+
+Comparison (==, ===, !=, !==, >, <, >=, <=)
+
+Logical (&&, ||, !)
+
+Ternary (? :)
+
+Unary (++, --, typeof, delete)
+
+Bitwise (&, |, ^, ~, <<, >>, >>>)
+3.What is the difference between == and ===?
+
+Answer: == checks for loose equality (values are compared after type coercion),
+ while === checks for strict equality (values and types must be the same).
+*/
